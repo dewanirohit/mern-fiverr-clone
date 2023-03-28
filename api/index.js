f58f4b8d35ab494257c4a6d9edab60923970396e
rohit <rohit.jd23@gmail.com>
2023-03-28 15:12:20 +0530

    fix api issus

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ const connect = async () => {
 	}
 };
 
-app.use(cors());
+app.use(cors({ origin: process.env.FRONT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
